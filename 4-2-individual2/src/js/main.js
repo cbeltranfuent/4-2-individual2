12e48d5f66c57938875e623650c84d0f5bb409aa
cbeltranfuent <cgbf20@gmail.com>
2023-05-26 03:41:46 -0400

    borro carpetas malas

--- 4-2-individual2/src/js/main.js
+++ 4-2-individual2/src/js/main.js
@@ -1,5 +1,32 @@
-import { articulos } from "./clases/articulos";
-import { proveedores } from "./clases/proveedores";
+import { articulos } from "../js/clases/articulos.js";
+import { proveedores } from "../js/clases/proveedores.js";
+import { setupCounter } from '../js/clases/counter.js';
+import '../css/style.css';
+// import './style.css'
+import javascriptLogo from '../../public/javascript.svg';
+// import javascriptLogo from './javascript.svg'
+import viteLogo from '../../public/vite.svg'
+// import viteLogo from '/vite.svg'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
 
 
 let articulo2 = new articulos("Arena de gato","20000");
